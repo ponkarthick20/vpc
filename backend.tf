331d487d91d6e76d2684004aa1c816961160ba2c
ponkarthick20 <45996932+ponkarthick20@users.noreply.github.com>
2025-04-25 12:48:23 +0100

    Update backend s3 bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "STATE_FILE_BUCKET_NAME"
+    bucket         = "my-tf-state"
     key            = "vpc/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "my-tf-lock"
